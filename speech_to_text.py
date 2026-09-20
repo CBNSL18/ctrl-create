8a20d2db7c21c5d950e686f0a67faba97167f1b1
CBNSL18 <star31030618@gmail.com>
2026-09-20 21:44:37 +0530

    complete speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,8 +1,6 @@
-```python
 from transformers import pipeline
 
 
-# Hugging Face Speech-to-Text model
 transcriber = pipeline(
     "automatic-speech-recognition",
     model="openai/whisper-large-v3"
@@ -10,6 +8,7 @@ transcriber = pipeline(
 
 
 def convert_speech_to_text(audio_file):
+
     result = transcriber(audio_file)
 
     text = result["text"]
@@ -17,10 +16,3 @@ def convert_speech_to_text(audio_file):
     print("Converted Text:", text)
 
     return text
-
-
-if __name__ == "__main__":
-    audio_file = input("Enter the audio file path: ")
-
-    convert_speech_to_text(audio_file)
-```
